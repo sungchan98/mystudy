b6ab7169fa3d5d6553ba5beaf48eb51a7e278158
sungchan98 <jvnhkdm0807@gmail.com>
2023-12-20 10:16:08 +0900

    1220

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level02/exam06/Main.java
@@ -0,0 +1,10 @@
+package algorithm.test.baekjoon.level02.exam06;
+
+public class Main {
+  public static void main(String[] args) {
+    java.util.Scanner keyIn = new java.util.Scanner(System.in);
+    int A = keyIn.nextInt();
+    int B = keyIn.nextInt();
+
+  }
+}
