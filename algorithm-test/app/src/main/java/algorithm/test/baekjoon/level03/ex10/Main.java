2e8d5fe850c80cd1e1a349bb93305318e6b8b2bd
sungchan98 <jvnhkdm0807@gmail.com>
2024-02-08 18:14:05 +0900

    0208

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/ex10/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/ex10/Main.java
@@ -7,11 +7,11 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n - i - 1; j++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < n - i; j++) {
         System.out.print(" ");
       }
-      for (int j = 0; j < i + 1; j++) {
+      for (int j = 0; j < i; j++) {
         System.out.print("*");
       }
       System.out.println();
--- algorithm-test/app/src/main/java/algorithm/test/codetree/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/codetree/Main.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     // 여기에 코드를 작성해주세요.
+
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
 
     for (int i = 0; i < n; i++) {
-      for (int j = 0; j < i; j++) {
-        System.out.print("  ");
+      for (int j = 0; j < n - (i + 1); j++) {
+        System.out.print(" ");
       }
-      for (int j = 0; j < (2 * n) - (2 * i) - 1; j++) {
+      for (int j = 0; j < i + 1; j++) {
         System.out.print("* ");
       }
       System.out.println();
     }
 
-
-    for (int i = 1; i <= n - 1; i++) {
-      for (int j = n - i; j > 1; j--)
-        System.out.print("  ");
-      for (int j = 0; j < 2 * i + 1; j++)
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < (i + 1); j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < n - i - 1; j++) {
         System.out.print("* ");
+      }
       System.out.println();
     }
   }
