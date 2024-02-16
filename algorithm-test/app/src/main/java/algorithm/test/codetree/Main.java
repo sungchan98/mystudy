7394c58134f6481fd8f33de2e62a40adf79992c4
sungchan98 <jvnhkdm0807@gmail.com>
2024-02-16 12:44:27 +0900

    0216

--- algorithm-test/app/src/main/java/algorithm/test/codetree/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/codetree/Main.java
@@ -11,7 +11,7 @@ public class Main {
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
         if (j % 2 == 0) {
-          j = 1;
+
           System.out.print("* ");
         } else {
           System.out.print("* ");
