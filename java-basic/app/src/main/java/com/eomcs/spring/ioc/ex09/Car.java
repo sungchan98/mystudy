d06cab4f95c42b164253d27d7e6ef44a08483cfc
sungchan98 <jvnhkdm0807@gmail.com>
2024-03-12 16:15:03 +0900

    0312

--- java-basic/app/src/main/java/com/eomcs/spring/ioc/ex09/Car.java
+++ java-basic/app/src/main/java/com/eomcs/spring/ioc/ex09/Car.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Component;
 // @Component
 // => 스프링 IoC 컨테이너는 이 애노테이션이 붙은 클래스에 대해 객체를 자동 생성한다.
 // 문법:
-//      @Component(value="객체이름")
-//      @Component("객체이름")
+// @Component(value="객체이름")
+// @Component("객체이름")
 // 만약 다음과 같이 객체의 이름을 생략하면
 // 클래스 이름을 객체 이름으로 사용한다.
-//      예) bitcamp.java106.step09.Car => "car"
+// 예) bitcamp.java106.step09.Car => "car"
 // 즉 클래스 이름에서 첫 알파벳을 소문자로 한 이름을
 // 객체 이름으로 사용한다.
 //
@@ -34,46 +34,50 @@ public class Car {
 
   @Override
   public String toString() {
-    return "Car [model=" + model + ", maker=" + maker + ", cc=" + cc + ", auto=" + auto + ", createdDate="
-        + createdDate + ", engine=" + engine + "]";
+    return "Car [model=" + model + ", maker=" + maker + ", cc=" + cc + ", auto=" + auto
+        + ", createdDate=" + createdDate + ", engine=" + engine + "]";
   }
+
   /*
-   * 의존 객체는 작업하는데 사용하라고 생성자를 호출할 때 외부에서 주입하는 객체이기 때문에
-   * 셋터나 겟터를 정의할 필요가 없다.
-    public Engine getEngine() {
-        return engine;
-    }
-    public void setEngine(Engine engine) {
-        this.engine = engine;
-    }
+   * 의존 객체는 작업하는데 사용하라고 생성자를 호출할 때 외부에서 주입하는 객체이기 때문에 셋터나 겟터를 정의할 필요가 없다. public Engine getEngine()
+   * { return engine; } public void setEngine(Engine engine) { this.engine = engine; }
    */
   public boolean isAuto() {
     return auto;
   }
+
   public void setAuto(boolean auto) {
     this.auto = auto;
   }
+
   public Date getCreatedDate() {
     return createdDate;
   }
+
   public void setCreatedDate(Date createdDate) {
     this.createdDate = createdDate;
   }
+
   public String getModel() {
     return model;
   }
+
   public void setModel(String model) {
     this.model = model;
   }
+
   public String getMaker() {
     return maker;
   }
+
   public void setMaker(String maker) {
     this.maker = maker;
   }
+
   public int getCc() {
     return cc;
   }
+
   public void setCc(int cc) {
     this.cc = cc;
   }
