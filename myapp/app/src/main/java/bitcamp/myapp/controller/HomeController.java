b66eeaca400d05e8326a179952d9cd1dab296a38
sungchan98 <jvnhkdm0807@gmail.com>
2024-03-26 15:21:19 +0900

    0326

--- myapp/app/src/main/java/bitcamp/myapp/controller/HomeController.java
+++ myapp/app/src/main/java/bitcamp/myapp/controller/HomeController.java
@@ -14,4 +14,14 @@ public class HomeController {
   public void home() { // url이 view 이름으로 사용됨
   }
 
+//  @GetMapping("/home1")
+//  public String home1() { // url이 view 이름으로 사용됨
+//    return "home.jsp";
+//  }
+//
+//  @GetMapping("/home2")
+//  public String home2() { // url이 view 이름으로 사용됨
+//    return "home.html";
+//  }
+
 }
