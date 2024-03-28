11458c14513f67c07114ba272a9517a8781eaca7
sungchan98 <jvnhkdm0807@gmail.com>
2024-03-28 12:13:04 +0900

    0328

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,10 +1,13 @@
 package bitcamp.myapp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
 
   public static void main(String[] args) throws Exception {
     System.out.println("과제관리 시스템 서버 실행!");
-    // SpringApplication.run(DemoApplication.class, args);
-
+    SpringApplication.run(App.class, args);
   }
 }
