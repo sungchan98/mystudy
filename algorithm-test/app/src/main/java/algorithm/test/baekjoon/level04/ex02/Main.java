9b4bbcec66b6b7b99c17567ca17f21f859324f89
sungchan98 <jvnhkdm0807@gmail.com>
2024-04-14 23:42:33 +0900

    0414

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/ex02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/ex02/Main.java
@@ -8,20 +8,12 @@ public class Main {
     int a = sc.nextInt();
     int b = sc.nextInt();
     int arr[] = new int[a];
-    int arr2[] = new int[a];
-    int cnt = 0;
+
     for (int i = 0; i < a; i++) {
       arr[i] = sc.nextInt();
       if (arr[i] < b) {
-        cnt++;
+        System.out.printf(arr[i] + " ");
       }
-    }
-
-    for (int i = 0; i < cnt; i++) {
-      if (arr[i] < b) {
-        arr2[i] = arr[i];
-      }
-      System.out.printf(arr2[i] + " ");
     }
   }
 }
