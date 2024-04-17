b9661a600729488f5e54c5634097a75ba6c0dc40
sungchan98 <jvnhkdm0807@gmail.com>
2024-04-17 12:03:03 +0900

    0417

--- algorithm-test/app/src/main/java/algorithm/test/codetree/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/codetree/Main.java
@@ -4,26 +4,38 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+    // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int idx = -1;
+    boolean c = true;
 
-        int arrA[] = new int[100];
-        int arrB[] = new int[100];
+    int arrA[] = new int[100];
+    int arrB[] = new int[100];
 
-        for(int i = 0; i < a; i++){
-            arrA[i] = sc.nextInt();
+    for (int i = 0; i < a; i++) {
+      arrA[i] = sc.nextInt();
+    }
+
+    for (int i = 0; i < b; i++) {
+      arrB[i] = sc.nextInt();
+    }
+
+    for (int i = 0; i < a; i++) {
+      if (arrA[i] != arrB[i]) {
+        idx = i + 1;
+        if (arrA[idx] == arrB[i]) {
+
         }
-        for(int i = 0; i < b; i++){
-            arrB[i] = sc.nextInt();
-            if(arrA[i] != arrB[i]){
-                arrA[i++];
-                System.out.printf(arrA + " ");
-            }else if(arrA[i] == arrB[i]){
-                
-            }
-        }
+      }
+    }
+
+    if (idx == -1) {
+      System.out.print("No");
+    } else {
+      System.out.print("Yes");
+    }
   }
 }
