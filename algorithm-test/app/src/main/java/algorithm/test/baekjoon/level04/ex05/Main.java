f261fd7c8b039b684aa636ea966cd590f5e0ac15
sungchan98 <jvnhkdm0807@gmail.com>
2024-04-21 23:43:53 +0900

    0421

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/ex05/Main.java
@@ -0,0 +1,5 @@
+package algorithm.test.baekjoon.level04.ex05;
+
+public class Main {
+
+}
--- algorithm-test/app/src/main/java/algorithm/test/codetree/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/codetree/Main.java
@@ -7,37 +7,24 @@ public class Main {
     // 여기에 코드를 작성해주세요.
     Scanner sc = new Scanner(System.in);
 
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    int idx = -1;
-    boolean c = false;
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    int a = 0;
+    final int INT_MAX = Integer.MAX_VALUE;
+    int minVal = INT_MAX;
 
-    int arrA[] = new int[100];
-    int arrB[] = new int[100];
-
-    for (int i = 0; i < a; i++) {
-      arrA[i] = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
     }
 
-    for (int i = 0; i < b; i++) {
-      arrB[i] = sc.nextInt();
-    }
+    for (int i = n - 1; i > 0; i--) {
 
-    for (int i = 0; i < a; i++) {
-      if (arrA[i] != arrB[i]) {
-        idx = i + 1;
-        if (arrA[idx] == arrB[i]) {
-          c = true;
-        } else {
-          c = false;
+      for (int j = i - 1; j >= 0; j--) {
+        if (arr[i] - arr[j] < minVal) {
+          minVal = arr[i] - arr[j];
         }
       }
     }
-
-    if (c = false) {
-      System.out.print("No");
-    } else {
-      System.out.print("Yes");
-    }
+    System.out.println(minVal);
   }
 }
