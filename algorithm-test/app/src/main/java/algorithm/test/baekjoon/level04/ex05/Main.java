5fbb84509f0afed90a4335e47885ee43d885b924
sungchan98 <jvnhkdm0807@gmail.com>
2024-04-23 14:04:11 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/ex05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/ex05/Main.java
@@ -1,5 +1,14 @@
 package algorithm.test.baekjoon.level04.ex05;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  
+  int N = sc.nextInt();
+  int M = sc.nextInt();
+  
+  int
+}
 }
