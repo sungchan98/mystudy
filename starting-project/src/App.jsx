25c9fb9a1cfb02f6d7b0ca2dd268e108b38c2fd5
sungchan98 <jvnhkdm0807@gmail.com>
2024-07-23 18:53:59 +0900

    리엑트 공부

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import PostList from "./components/PostList";
+import MainHeader from "./components/MainHeader";
+
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
   return (
-    <main>
-      <PostList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostList isPosting={modalIsVisible} onStopPosting={hideModalHandler} />
+      </main>
+    </>
   );
 }
 
--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
+
 import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./PostList.module.css";
 
-function PostList() {
+function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="React.js is awesom!" />
-      <Post author="Manuel" body="Check out the full course!" />
-    </ul>
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>포스트가 없습니다</h2>
+          <p>포스트를 작성해 보세요</p>
+        </div>
+      )}
+    </>
   );
 }
 
