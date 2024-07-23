728cbd4adf15dbd00d636af5aac1e8d2a0af4049
sungchan98 <jvnhkdm0807@gmail.com>
2024-07-23 20:13:50 +0900

    리엑트 공부중

--- starting-project/frontend/src/components/PostList.jsx
+++ starting-project/frontend/src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -8,7 +8,23 @@ import classes from "./PostList.module.css";
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
