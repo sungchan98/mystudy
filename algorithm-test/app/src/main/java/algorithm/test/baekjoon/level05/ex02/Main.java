5c2423bc24d1a247ea6aa6dd455841d0bb51b56c
sungchan98 <jvnhkdm0807@gmail.com>
2024-09-15 02:46:30 +0900

    백준2743번 풀이

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/ex02/Main.java
@@ -0,0 +1,14 @@
+package algorithm.test.baekjoon.level05.ex02;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    String s = sc.next();
+
+
+    System.out.println(s.length());
+  }
+}
