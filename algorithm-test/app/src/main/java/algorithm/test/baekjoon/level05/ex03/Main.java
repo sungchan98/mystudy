25d5ba7cc1cf2e50cab2fb5b29eba258228758f0
sungchan98 <jvnhkdm0807@gmail.com>
2024-09-16 21:23:04 +0900

    백준9086번 풀이

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/ex03/Main.java
@@ -0,0 +1,18 @@
+package algorithm.test.baekjoon.level05.ex03;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    for (int i = 0; i < n; i++) {
+      String s = sc.next();
+      System.out.print(s.charAt(0));
+      System.out.println(s.charAt(s.length() - 1));
+    }
+
+  }
+}
