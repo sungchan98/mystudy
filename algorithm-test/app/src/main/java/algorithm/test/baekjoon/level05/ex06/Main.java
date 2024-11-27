9966866e2b8aefe6536993935d3f9afb159ce069
sungchan98 <jvnhkdm0807@gmail.com>
2024-11-27 15:10:39 +0900

    백준5단계7번문제 풀이

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/ex06/Main.java
@@ -0,0 +1,18 @@
+package algorithm.test.baekjoon.level05.ex06;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+
+    String s = sc.next();
+    int arr[] = new int[26];
+    for (int i = 0; i < 26; i++) {
+      if() {
+        arr[i] = s.indexOf(i);
+      }
+    }
+  }
+}
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/ex07/Main.java
@@ -0,0 +1,23 @@
+package algorithm.test.baekjoon.level05.ex07;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+
+    for (int i = 0; i < t; i++) {
+      int n = sc.nextInt();
+      String s = sc.next();
+      String p = "";
+      for (int j = 0; j < s.length(); j++) {
+        for (int k = 0; k < n; k++) {
+          p += s.charAt(j);
+        }
+      }
+      System.out.print(p);
+    }
+  }
+}
