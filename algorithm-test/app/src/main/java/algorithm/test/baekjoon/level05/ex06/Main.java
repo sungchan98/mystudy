830cbc5682f5f33f1039f39e24ddec97ac61f92a
sungchan98 <jvnhkdm0807@gmail.com>
2024-12-27 22:34:24 +0900

    백준level6 1번 풀이

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/ex06/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/ex06/Main.java
@@ -7,12 +7,25 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
 
-    String s = sc.next();
+    String s = sc.nextLine();
+
     int arr[] = new int[26];
+
     for (int i = 0; i < 26; i++) {
-      if() {
-        arr[i] = s.indexOf(i);
+      arr[i] = -1;
+    }
+
+
+    for (int i = 0; i < s.length(); i++) {
+      int idx = s.charAt(i) - 'a';
+
+      if (arr[idx] == -1) {
+        arr[idx] = i;
       }
+    }
+
+    for (int i = 0; i < 26; i++) {
+      System.out.print(arr[i] + " ");
     }
   }
 }
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/ex01/Main.java
@@ -0,0 +1,5 @@
+package algorithm.test.baekjoon.level06.ex01;
+
+public class Main {
+
+}
